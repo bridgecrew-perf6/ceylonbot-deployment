9f4fbba6a4baade8ff9674585c7e2621b880c47e
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-12-02 13:57:53 -0600

    update]

--- lex-lambda/lookingFor.js
+++ lex-lambda/lookingFor.js
@@ -43,252 +43,212 @@ function dispatch(intentRequest, callback) {
     const sessionAttributes = intentRequest.sessionAttributes;
     const slots = intentRequest.currentIntent.slots;
     const interestingQuestion = slots.interestingQuestion;
-    const lookingResponse = slots.lookingResponse;
     const giftResponse = slots.giftResponse;
     const yesNo = slots.yesNo;
-    
+
     // const lookingFor = slots.lookingFor; //User Input
     if (interestingQuestion === "I'm not sure") {
-        if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+        callback(close(sessionAttributes, 'Fulfilled',
+        {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
+
     } else if (interestingQuestion === "I don't know") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
     } else if (interestingQuestion === "Tell me what I need") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
     } else if (interestingQuestion === "I can figure it out") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+        
     } else if (interestingQuestion === "I can lead ") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+        
     } else if (interestingQuestion === "I know what I need") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+        
     } else if (interestingQuestion === "Tell me") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+        
     } else if (interestingQuestion === "You can lead me") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+        
     } else if (interestingQuestion === "what would you recommend") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+        
     } else if (interestingQuestion === "Tell me") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+       
     } else if (interestingQuestion === "You are the one") {
-        if (lookingResponse === "I'm not sure") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (lookingResponse === "I'm sure") {
-            callback(close(sessionAttributes, 'Fulfilled',
-            {'contentType': 'PlainText', 'content': `Well if you need more options respond with "other" for more options`}));
-        }
+    
     }
     
     
     if (giftResponse === "love") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "electronics") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "skin care") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "book") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "drinks") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "bag") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "appreciation") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "food") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "products") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "vehicle") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "admiration") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "money") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "car") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "phone") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "furniture") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "dog") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     } else if (giftResponse === "pet") {
-        if (yesNo === "I'm sure") {
+        if (yesNo === "Yes") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Hmmm...let me take a look at what I can find for you.\n Respond with "facial products" for our products or "other" for more options`}));
-        } else if (yesNo === "I'm not sure") {
+        } else if (yesNo === "No") {
             callback(close(sessionAttributes, 'Fulfilled',
             {'contentType': 'PlainText', 'content': `Respond with "facial products" for our products or "other" for more options`}));
         }
     }
 
-
-
 }
 // Route the incoming request based on intent.
 // The JSON body of the request is provided in the event slot.
-exports.handler = (event, context, callback) => {
-    try {
-        dispatch(event,
-            (response) => {
-                callback(null, response);
-            });
-    } catch (err) {
-        callback(err);
-    }
+// exports.handler = (event, context, callback) => {
+//     try {
+//         console.log(`event.bot.name=${event.bot.name}`);
+//         dispatch(event, (response) => {callback(null, response);});
+//     } catch (err) {
+//         callback(err);
+//     }
+// };
+
+module.exports.intents = (event, context, callback) => {
+  try {
+    console.log(`event.bot.name=${event.bot.name}`);
+    dispatch(event).then(response => {
+      callback(null, response);
+    });
+  } catch (err) {
+    callback(err);
+  }
 };
